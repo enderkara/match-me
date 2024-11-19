b3e73e187be426d25f02e58090c8069d38efed31
Ender Karagul <111353595+enderkara@users.noreply.github.com>
2024-11-19 22:35:33 +0000

    loginForm Created

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Button,
   Card,
@@ -9,38 +10,39 @@ from "@nextui-org/react";
 import React from "react";
 import { GiPadlock } from "react-icons/gi";
 import { useForm, SubmitHandler } from "react-hook-form";
-
+console.log(useForm); // Check if this outputs a function
 
 export default function LoginForm() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data: any) => {
-    console.log(data);
-  };
+  const { 
+    register,
+    handleSubmit,
+    formState:{isValid,errors},
 
+  } = useForm();
+  const onSubmit = (data: any) => 
+    console.log(data);
+  
   return (
     <Card className="w-2/5 mx-auto">
       <CardHeader className="flex flex-col items-center justify-center">
         <div className="flex flex-col gap-2 items-center text-default">
           <div className="flex flex-row items-center gap-3">
             <GiPadlock size={30} />
-            <h1 className="text-3xl font-semibold">
-              Login
-            </h1>
+            <h1 className="text-3xl font-semibold">Login</h1>
           </div>
-          <p className="text-neutral-500">
-            Welcome back to MatchMe!
-          </p>
+          <p className="text-neutral-500">Welcome back to MatchMe!</p>
         </div>
       </CardHeader>
       <CardBody>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="space-y-4">
             <Input
               defaultValue=""
               label="Email"
               variant="bordered"
-              {...register("Email")}
-
+              {...register("Email", {
+                required:"Email is required",
+              })}
             />
             <Input
               defaultValue=""
@@ -49,14 +51,9 @@ export default function LoginForm() {
               type="password"
               {...register("Password")}
             />
-            <Button
-              fullWidth
-              color="default"
-              type="submit"
-            >
+            <Button fullWidth color="default" type="submit">
               Login
             </Button>
-
           </div>
         </form>
       </CardBody>
